91752eff8856e723128780e1dd033f07e8a7d92a
Stas Vorozhun <stanislav.vorozhun@gmail.comn>
2022-10-03 13:10:52 +0200

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 let a = `Nigga`
 console.log('hello, '+a)
 console.log(`Hello ,${a}`)
+console.log('hello, '+a)
+console.log(`Hello ,${a}`)
+console.log('hello, '+a)
+console.log(`Hello ,${a}`)
+console.log('hello, '+a)
+console.log(`Hello ,${a}`)
