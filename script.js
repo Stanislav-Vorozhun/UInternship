16c4514539882b67d71aa82df8d65d114101a5e4
Stas Vorozhun <stanislav.vorozhun@gmail.comn>
2022-10-05 09:32:33 +0200

    Task 1

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict'
 
-let a = `Nigga`
-console.log('hello, '+a)
-console.log(`Hello ,${a}`)
-console.log('hello, '+a)
-console.log(`Hello ,${a}`)
-console.log('hello, '+a)
-console.log(`Hello ,${a}`)
-console.log('hello, '+a)
-console.log(`Hello ,${a}`)
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? ', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+}
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("Один из последних просмотренных фильмов?", "");
+
+  personalMovieDB.movies[a]=b;
+  personalMovieDB.movies[c]=d;
