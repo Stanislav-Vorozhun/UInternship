89e515e020c1fc72d4c6544777daa08abfb9c066
Stas Vorozhun <stanislav.vorozhun@gmail.comn>
2022-10-05 17:36:01 +0200

    repeating conditions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? ', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? ', '')
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,10 +10,26 @@ const personalMovieDB = {
   private: false
 }
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("Один из последних просмотренных фильмов?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-  personalMovieDB.movies[a]=b;
-  personalMovieDB.movies[c]=d;
+  a != '' && b != null && a != null && b != '' && a.length < 50 ? personalMovieDB.movies[a] = b : i--;
+} 
+
+if (personalMovieDB.count<10){
+  console.log('Просмотрено довольно мало фильмов')
+}
+else if ( personalMovieDB.count>=10 && personalMovieDB.count<30){
+  console.log('Вы классический зритель')
+}
+else if ( personalMovieDB.count>=30) {
+  console.log('Вы киноман')
+}
+else {
+  console.log('Произошла ошибка...')
+}
+
+console.log(personalMovieDB);
+
+
